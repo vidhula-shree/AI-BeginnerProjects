9de0de1758591b3543d2eb9a68c562a361be5bd4
vidhula-shree <99156725+vidhula-shree@users.noreply.github.com>
2026-01-29 18:26:46 +0530

    Movie recommendation System
    Developed an Unsupervised Learning system using Content based filtering on the TMDB 5000 dataset. I engineered a Tags feature by merging overviews, genres, and credits metadata. Applied Count vectorizer to transform text into a 5,000 dimensional vector space and used Cosine Similarity to calculate mathematical distance between films.

---
+++ Movie-Recommender/project3.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import ast
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+movies = pd.read_csv('tmdb_5000_movies.csv')
+credits = pd.read_csv('tmdb_5000_credits.csv')
+movies = movies.merge(credits, on='title')
+movies = movies[['movie_id', 'title', 'overview', 'genres', 'keywords', 'cast', 'crew']]
+movies.dropna(inplace=True)
+
+def convert(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        L.append(i['name'])
+    return L
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+movies['cast'] = movies['cast'].apply(lambda x: [i['name'] for i in ast.literal_eval(x)[:3]]) # Top 3 actors
+
+def fetch_director(obj):
+    L = []
+    for i in ast.literal_eval(obj):
+        if i['job'] == 'Director':
+            L.append(i['name'])
+            break
+    return L
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+movies['genres'] = movies['genres'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['keywords'] = movies['keywords'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['cast'] = movies['cast'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['crew'] = movies['crew'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['overview'] = movies['overview'].apply(lambda x:x.split())
+
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+new_df = movies[['movie_id', 'title', 'tags']]
+new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x).lower())
+
+#main logic 
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(new_df['tags']).toarray()
+similarity = cosine_similarity(vectors)
+
+def recommend(movie):
+    try:
+        movie_index = new_df[new_df['title'] == movie].index[0]
+        distances = similarity[movie_index]
+        movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+        
+        print(f"Movies similar to '{movie}':")
+        for i in movies_list:
+            print(f"- {new_df.iloc[i[0]].title}")
+    except IndexError:
+        print("Movie not found in dataset.")
+
+recommend('Avatar')
